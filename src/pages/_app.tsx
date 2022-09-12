a3eceb85fd14f7f67d7483741b2c0a28b4cbe9f2
Carlos Benavides <carlosbvz@gmail.com>
2022-09-11 21:18:00 -0600

    Amplify update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,8 @@
 import "../styles/globals.css";
 import { GameProvider } from "../contexts/GameContext";
+import { Amplify, Auth } from "aws-amplify";
+import awsconfig from "../aws-exports";
+Amplify.configure(awsconfig);
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
